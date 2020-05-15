5dae43ff288fcb66c4123a334439d1a3c57e205e
Mani1211 <smanivasagamtnj@gmail.com>
2020-05-15 17:05:35 +0530

    ....

--- website/settings.py
+++ website/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDAI_ROOT = os.path.join(BASE_DIR,'media')
